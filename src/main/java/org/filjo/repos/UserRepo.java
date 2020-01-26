bfde9c8c3b9d9edd43971aa107b1572c5b7b5f01
Philipp <55308835+Filjo0@users.noreply.github.com>
2020-01-26 12:17:15 +0800

    initial commit

---
+++ src/main/java/org/filjo/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package org.filjo.repos;
+
+import org.filjo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
